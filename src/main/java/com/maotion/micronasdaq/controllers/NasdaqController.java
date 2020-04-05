86b46d9a4815b9d80d563e241deb368164904d94
mao <maoyuan@gmail.com>
2020-04-04 23:01:30 -0400

    cloud bus

--- src/main/java/com/maotion/micronasdaq/controllers/NasdaqController.java
+++ src/main/java/com/maotion/micronasdaq/controllers/NasdaqController.java
@@ -1,22 +1,28 @@
 package com.maotion.micronasdaq.controllers;
 
 import com.maotion.micronasdaq.services.NasdaqDataService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NasdaqController {
-
+    private Logger logger = LogManager.getLogger(NasdaqController.class);
+    Environment environment;
     private NasdaqDataService nasdaqDataService;
 
     @Autowired
-    public NasdaqController(NasdaqDataService nasdaqDataService) {
+    public NasdaqController(NasdaqDataService nasdaqDataService,Environment environment) {
         this.nasdaqDataService = nasdaqDataService;
+        this.environment =environment;
     }
 
     @GetMapping("/nasdaq")
     public String nasdaqXQC() {
+        logger.debug(String.format("--------the bus.refresh.test value is : %s --------",environment.getProperty("bus.refresh.test")));
         return nasdaqDataService.getQuote();
     }
 }
